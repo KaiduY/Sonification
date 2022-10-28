4d65c95089bb619c344833149456ffcd1a2b80db
KaiduY <alex.oprea2003@gmail.com>
2022-10-28 22:03:25 +0200

    trying a high pass filter

--- analyze.py
+++ analyze.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from data import example
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, ifft
+from scipy.signal import butter
 
 smrate, data = example(n=1)
 
@@ -11,11 +12,20 @@ N = len(mono)
 yf = fft(mono)
 xf = fftfreq(N, 1 / smrate)
 
+#print(yf)
 
+cutoff = 10 * 1e3
+#High pass filter
+for i, sample in enumerate(yf):
+    if abs(sample) < cutoff:
+        #print(abs(sample))
+        yf[i] = 0
+
+sig = ifft(yf)
 
 plt.figure(1)
 c = plt.subplot()
-Pxx, freqs, bins, im = c.specgram(mono, NFFT=2**10, Fs=smrate, noverlap=50)
+Pxx, freqs, bins, im = c.specgram(sig, NFFT=2**10, Fs=smrate, noverlap=50)
 c.set_xlabel('Time')
 c.set_ylabel('Frequency')
 plt.show()
