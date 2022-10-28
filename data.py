6f6aca6082978c61f3f8e4f4b7d8ac985f56af5d
KaiduY <alex.oprea2003@gmail.com>
2022-10-28 11:20:25 +0200

    Data parser and one exempe added

---
+++ data.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from scipy.io import wavfile
+from random import choice
+import glob
+
+project_path  = Path(__file__).parent
+
+def exemple(random = False, n = 0):
+    files = glob.glob(glob.escape(project_path) + "/Data/*.wav")
+    if random:
+        file = choice(files)
+    elif n < len(files):
+        file = files[n]
+    else:
+        raise Exception(f'There are only {len(files)} exemples available!')
+    
+    return wavfile.read(file)
+
+
+if __name__=='__main__':
+    print('You are not supposed to run this file, did something went wrong?')
