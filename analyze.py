139bd12c09b870fcbe7db482064ec3d9a1d103a7
KaiduY <alex.oprea2003@gmail.com>
2022-11-20 16:14:30 +0100

    The last Pac version

--- analyze.py
+++ analyze.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from data import example
 from scipy.fft import fft, fftfreq, ifft
-from scipy.signal import butter
+from scipy.signal import butter, find_peaks
 
 smrate, data = example(n=1)
 
@@ -23,11 +23,23 @@ for i, sample in enumerate(yf):
 
 sig = ifft(yf)
 
+pk = find_peaks(sig)
+
+hpk = []
+for pek in pk:
+    print(pek)
+    abvalue = abs(sig[pek])
+    hpk.append(abvalue)
 plt.figure(1)
-c = plt.subplot()
-Pxx, freqs, bins, im = c.specgram(sig, NFFT=2**10, Fs=smrate, noverlap=50)
-c.set_xlabel('Time')
-c.set_ylabel('Frequency')
+
+peak = plt.subplot()
+peak.plot(sig)
+#peak.plot(pk, '*')
+print(hpk)
+#c = plt.subplot()
+#Pxx, freqs, bins, im = c.specgram(sig, NFFT=2**10, Fs=smrate, noverlap=50)
+#c.set_xlabel('Time')
+#c.set_ylabel('Frequency')
 plt.show()
 
 
