83c996d9c1fa2ddd1bd6970a662d1add471316aa
KaiduY <alex.oprea2003@gmail.com>
2022-11-21 19:51:42 +0100

    Made the peak detection alg work

--- analyze.py
+++ analyze.py
@@ -9,41 +9,32 @@ smrate, data = example(n=1)
 #Convert data to mono
 mono = data.sum(axis = 1)
 N = len(mono)
-yf = fft(mono)
-xf = fftfreq(N, 1 / smrate)
 
-#print(yf)
-
-cutoff = 10 * 1e3
-#High pass filter
-for i, sample in enumerate(yf):
-    if abs(sample) < cutoff:
-        #print(abs(sample))
-        yf[i] = 0
-
-sig = ifft(yf)
-
-pk = find_peaks(sig)
+pk, _ = find_peaks(mono)
 
 hpk = []
 for pek in pk:
-    print(pek)
-    abvalue = abs(sig[pek])
-    hpk.append(abvalue)
+    hpk.append(mono[pek])
+
 plt.figure(1)
 
 peak = plt.subplot()
-peak.plot(sig)
-#peak.plot(pk, '*')
-print(hpk)
+peak.plot(mono)
+peak.plot(pk, hpk, 'r*')
+plt.show()
+
+#QUESTION WHICH PEAKS ARE ACTUALLY STARS? WE NEED MORE DATA BRUH ----> PUSH FASTER THE DATA GENERATION PART
+
+
+
+#HOW TO DO A SPECTOGRAM/WATERFALL + FFT
+
+#yf = fft(mono)
+#xf = fftfreq(N, 1 / smrate)
+#sig = ifft(yf)
+
+
 #c = plt.subplot()
 #Pxx, freqs, bins, im = c.specgram(sig, NFFT=2**10, Fs=smrate, noverlap=50)
 #c.set_xlabel('Time')
 #c.set_ylabel('Frequency')
-plt.show()
-
-
-#plt.figure(1)
-#plt.title('Waveform')
-#plt.plot(xf, np.abs(yf))
-#plt.show()
